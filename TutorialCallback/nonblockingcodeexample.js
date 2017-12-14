f73bef79e38208f332f99958f1b083343df71c89
Andreas Grossbauer <agrossbauer@globalrefund.local>
2017-12-14 17:22:56 +0100

    initial callback implementation

---
+++ TutorialCallback/nonblockingcodeexample.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+
+fs.readFile('input.txt', 
+	function (err, data)
+	{
+		if (err)
+		{
+		return console.error(err);
+		}
+		else
+		{
+		console.log(data.toString());
+		}
+	}
+);
+
+console.log("Program Ended");
